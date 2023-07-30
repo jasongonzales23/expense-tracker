197c365f8c9441fa19ce7d9ffeda4d3130636216
jasongonzales23 <jason.gonzales23@gmail.com>
2023-07-29 18:26:55 -0700

    query all from current month

--- app/api/get-expenses/route.js
+++ app/api/get-expenses/route.js
@@ -4,10 +4,17 @@ import { NextResponse } from "next/server";
 
 export async function GET(request) {
   let expenses;
-  let startTime = Date.now();
+  const today = new Date();
+  const firstday = new Date(today.getFullYear(), today.getMonth(), 1);
+  const lastday = new Date(today.getFullYear(), today.getMonth() + 1, 0);
 
   try {
-    expenses = await db.selectFrom("expenses").selectAll().execute();
+    expenses = await db
+      .selectFrom("expenses")
+      .selectAll()
+      .where("createdAt", ">=", firstday)
+      .where("createdAt", "<=", lastday)
+      .execute();
   } catch (e) {
     if (e.message === `relation "expenses" does not exist`) {
       console.log(
@@ -15,7 +22,6 @@ export async function GET(request) {
       );
       // Table is not created yet
       await seed();
-      startTime = Date.now();
       expenses = await db.selectFrom("expenses").selectAll().execute();
     } else {
       throw e;
