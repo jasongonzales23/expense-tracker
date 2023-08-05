2660588d7f29a764d987e78361b9d344893444e1
Jason <jason@devzero.io>
2023-08-05 21:53:03 +0000

    add flowbite and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,7 @@ export default function Home() {
   const total = expenses?.reduce((sum: number, { amount }) => sum + amount, 0);
   return (
     <main className="container p-4 md:p-10 mx-auto max-w-7xl">
-      <Title className="mb-4">Expense Tracker</Title>
       <Total className="mb-4" total={total} />
-
       <AddExpenseForm mutate={mutate} />
     </main>
   );
