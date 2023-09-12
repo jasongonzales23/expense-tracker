2b225ae4d6da7f3e27290ffe84d5c8556e624f15
jasongonzales23 <jason.gonzales23@gmail.com>
2023-09-11 17:19:46 -0700

    first cut at adding auth0

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Flowbite } from "flowbite-react";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 import Navigation from "@/components/Navigation";
 import customTheme from "./custom-theme";
 import "./globals.css";
@@ -11,12 +12,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="h-full bg-gray-50">
-        <Flowbite theme={{ theme: customTheme }}>
-          <Navigation />
-          {children}
-        </Flowbite>
-      </body>
+      <UserProvider>
+        <body className="h-full bg-gray-50">
+          <Flowbite theme={{ theme: customTheme }}>
+            <Navigation />
+            {children}
+          </Flowbite>
+        </body>
+      </UserProvider>
     </html>
   );
 }
