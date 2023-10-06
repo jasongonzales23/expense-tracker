4cd05516665685e63358ffece481365e60965397
jasongonzales23 <jason.gonzales23@gmail.com>
2023-10-06 10:10:57 -0700

    add first test

---
+++ __tests__/Form.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import { describe } from "node:test";
+import AddExpenseForm from "@/components/AddExpenseForm";
+
+describe("Fill out form", () => {
+  it("should work", () => {
+    render(<AddExpenseForm mutate={() => {}} />);
+    expect(screen.getByText("Save Expense")).toBeInTheDocument();
+  });
+});
