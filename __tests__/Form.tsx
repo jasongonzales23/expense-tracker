5adfdf32620a55c638612034e862787838b2e71c
jasongonzales23 <jason.gonzales23@gmail.com>
2023-10-06 10:20:48 -0700

    add more testing config

--- __tests__/Form.tsx
+++ __tests__/Form.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
-import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import { describe } from "node:test";
 import AddExpenseForm from "@/components/AddExpenseForm";
